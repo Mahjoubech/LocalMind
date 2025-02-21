c8151225675e7c4f1d8ed881b6331b1f2ba452dc
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-02-21 05:15:16 +0100

    create fonctionalité for login and authenticate and save in class Auth and sesseion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,9 @@ class AuthController extends Controller
     public function register(){
         return view('auth.register');
     }
-
+    public function login(){
+        return view('auth.login');
+    }
     public function store(){
         //validation
         //create user
@@ -20,7 +22,7 @@ class AuthController extends Controller
         $validated = request()->validate([
               'name' => 'required|min:3|max:40',
               'email' => 'required|email|unique:users,email',
-              'password' => 'required|confirmed',
+              'password' => 'required|confirmed|min:8',
 
         ]);
 
@@ -30,5 +32,26 @@ class AuthController extends Controller
               'password' => Hash::make($validated['password'])
         ]);
         return redirect()->route('qstHome')->with('success','Account created Successfuly !!');
+    }
+    public function authenticate(){
+        dd(request()->all());
+        $validated = request()->validate([
+              'email' => 'required|email',
+              'password' => 'required|min:8',
+
+        ]);
+        if(Auth :: attempt($validated)){
+            request()->session()->regenerate();
+            return redirect()->route('qstHome')->with('success','Account login Successfuly !!');
+        }
+
+        User::create([
+          'name' => $validated['name'],
+              'email' => $validated['email'],
+              'password' => Hash::make($validated['password'])
+        ]);
+        return redirect()->route('login')->withErrors([
+            'email' => 'No matching user found with the provided email and password'
+        ]);
     }
 }
