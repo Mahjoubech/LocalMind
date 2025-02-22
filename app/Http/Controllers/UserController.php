9ac60160a001f4499543a2b6414fe60b896b495b
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-02-22 20:53:24 +0100

    add logique for edit profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,26 +3,53 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
 
 
 class UserController extends Controller
 {
-    public function show(User $user){
-       $qstuser = $user->question()->paginate(3);
-        return view('users.showProfile',compact('user','qstuser'));
+
+    public function show(User $user)
+    {
+        $qstuser = $user->question()->paginate(3);
+        return view('users.showProfile', compact('user', 'qstuser'));
     }
-    public function edit(User $user){
+    public function edit(User $user)
+    {
         $editing = true;
-       $qstuser = $user->question()->paginate(3);
+        $qstuser = $user->question()->paginate(3);
 
-        return view('users.showProfile',compact('user','editing','qstuser'));
+        return view('users.editProfile', compact('user', 'editing', 'qstuser'));
     }
-    public function update(){
+    public function update(User $user)
+    {
+        $validate = request()->validate([
+            'name' => 'required|min:3|max:40',
+            'email'  => 'required|email',
+            'location' => 'required',
+            'image' => 'image',
+            'about' => 'required|min:1|max:255'
 
+        ]);
+
+        if (request()->has('image')) {
+
+            if (!empty($user->profile_picture)) {
+                $deleted = Storage::disk('public')->delete($user->profile_picture);
+                if (!$deleted) {
+                    echo 'fuck of';
+                }
+            }
+            $imagePath = request()->file('image')->store('profile', 'public');
+            $validate['profile_picture'] = $imagePath;
+        }
+        $user->update($validate);
+        return redirect()->route('profile');
     }
 
-    public function profile(){
+    public function profile()
+    {
 
         $user =  Auth::user();
         return $this->show($user);
