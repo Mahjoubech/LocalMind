6f25940254c32565db26f03315ae508b535f9346
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-02-23 01:44:13 +0100

    send message in emmail for thinks joing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Mail\WelcomeEmail;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
@@ -31,6 +32,7 @@ class AuthController extends Controller
               'email' => $validated['email'],
               'password' => Hash::make($validated['password'])
         ]);
+        Mail::to(request()->user()->email)->send(new WelcomeEmail(request()->user()));
         return redirect()->route('qstHome')->with('success','Account created Successfuly !!');
     }
     public function authenticate(){
